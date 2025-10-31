266268977aeb225c44e9b30ce0f84d2e60ee9d00
Alibek Mukhammadiev <97537819+mohammedaliyy@users.noreply.github.com>
2025-10-31 23:34:14 +0500

    routes done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,23 @@
+import React from "react";
+import { Routes, Route } from "react-router";
 
+// import pages
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import UserDashboard from "./pages/UserDashboard";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="max-w-4xl mx-auto my-5 px-4 bg-amber-50">
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/dashboard" element={<UserDashboard />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;

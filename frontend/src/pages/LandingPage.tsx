b7b0f299a2f77a5fde2326cfdbfda95324f30009
Alibek Mukhammadiev <97537819+mohammedaliyy@users.noreply.github.com>
2025-11-07 11:28:34 +0500

    Header done

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,29 +1,73 @@
-import React from "react";
-import logo from "../assets/logo.png";
+import React, { useState } from "react";
+
+// navigate function
+import { useNavigate } from "react-router";
+
+// react-icons
+import { MdLanguage } from "react-icons/md";
+import { MdDarkMode } from "react-icons/md";
+import { MdLightMode } from "react-icons/md";
 
 export default function LandingPage() {
+  const navigate = useNavigate();
   return (
-    <div>
-      <header className="flex">
+    <div className="px-4">
+      <header className="flex items-center justify-between py-5">
+        {/* Logo */}
         <div>
           <a href="#">
-            <img src={logo} alt="Company Logo" />
+            <span className="font-league-spartan font-black text-xl">
+              wordify.
+            </span>
           </a>
         </div>
-        <ul>
+        {/* Navigations */}
+        <ul className="list-none flex gap-5">
           <li>
-            <a href="#home">Home</a>
+            <a className="hover:text-gray-300 transition-colors" href="#home">
+              Home
+            </a>
           </li>
           <li>
-            <a href="#about">About</a>
+            <a className="hover:text-gray-300 transition-colors" href="#about">
+              About
+            </a>
           </li>
           <li>
-            <a href="#features">Features</a>
+            <a
+              className="hover:text-gray-300 transition-colors"
+              href="#features"
+            >
+              Features
+            </a>
           </li>
         </ul>
-        <div>
-          <button>Login</button>
-          <button>Sign Up</button>
+        {/* Modes */}
+        <div className=" border-[1.5px] rounded-lg">
+
+        <div className="flex gap-2 p-1">
+          {false ? (
+            <MdDarkMode className="size-7 p-0.5  bg-white text-blue-500 duration-100 ease-in hover:cursor-pointer  hover:bg-gray-300 rounded-sm" />
+          ) : (
+            <MdLightMode className="size-8 p-1 bg-white text-amber-500 duration-100 ease-in hover:cursor-pointer  hover:bg-gray-300 rounded-sm" />
+          )}
+          <MdLanguage className="size-8 p-1  text-blue-500 bg-white rounded-sm" />
+        </div></div>
+
+        {/* Buttons */}
+        <div className="flex gap-5">
+          <button
+            className="text-gray-300 hover:cursor-pointer hover:text-gray-400 duration-100 ease-in"
+            onClick={() => navigate("/login")}
+          >
+            Login
+          </button>
+          <button
+            className="hover:cursor-pointer hover:text-gray-200 hover:bg-teal-600 bg-teal-500 duration-100 ease-in px-4.5 py-1.5 rounded-sm"
+            onClick={() => navigate("/register")}
+          >
+            Sign Up
+          </button>
         </div>
       </header>
     </div>
